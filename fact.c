8f7a5e7b04463d7d02500e6fd5012fc626f9f0df
EC2 Default User <ec2-user@ip-172-31-16-10.eu-north-1.compute.internal>
2025-09-16 08:41:33 +0000

    factorial

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long long fact(int n) {
+    if (n < 0) return -1;
+    long long f = 1;
+    for (int i = 2; i <= n; ++i) f *= i;
+    return f;
+}
+
+int main(void) {
+    int n;
+    if (scanf("%d", &n) != 1) return 1;
+    long long f = fact(n);
+    if (f < 0) { puts("invalid"); return 1; }
+    printf("%lld\n", f);
+    return 0;
+}
