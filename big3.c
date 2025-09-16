a6f84f30fa3b2b01387e283afcd634b43e862fd4
EC2 Default User <ec2-user@ip-172-31-16-10.eu-north-1.compute.internal>
2025-09-16 08:36:02 +0000

    biggest of 3

---
+++ big3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    if (scanf("%d %d %d", &a, &b, &c) != 3) return 1;
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    printf("%d\n", max);
+    return 0;
+}
